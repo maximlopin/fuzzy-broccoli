2978d012b2a61ef5523604a70e60a5baff7adf98
Maxim Lopin <wzzhz13@gmail.com>
2022-09-03 14:18:32 +0300

    fix

--- generate_requests.sh
+++ generate_requests.sh
@@ -1,4 +1,4 @@
-for i in {1..50}; do curl -o /dev/null -s http://localhost:8080/200; done;
-for i in {1..40}; do curl -o /dev/null -s http://localhost:8080/300; done;
-for i in {1..30}; do curl -o /dev/null -s http://localhost:8080/400; done;
-for i in {1..20}; do curl -o /dev/null -s http://localhost:8080/500; done;
+for i in {1..50}; do curl -o /dev/null -s http://localhost:80/200; done;
+for i in {1..40}; do curl -o /dev/null -s http://localhost:80/300; done;
+for i in {1..30}; do curl -o /dev/null -s http://localhost:80/400; done;
+for i in {1..20}; do curl -o /dev/null -s http://localhost:80/500; done;
